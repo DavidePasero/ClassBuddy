1cd583b8579c27045962723894e6cd0b80135d6c
21sarage <106278056+21sarage@users.noreply.github.com>
2023-12-28 09:19:06 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -17,7 +17,7 @@
     </title>
     <link rel="icon" href="../img/image.x" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="../style/home.css">
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width", initial-scale="1.0">
 </head>
 <body>
     <header>
